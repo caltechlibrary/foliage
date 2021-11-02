8cf115066a0f7bc35589b4ac873338c808cf7826
mhucka <mhucka@caltech.edu>
2021-11-01 19:28:54 -0700

    Move Enum utility classes to a separate file

---
+++ foliage/enum_utils.py
@@ -0,0 +1,26 @@
+'''
+enum_utils.py: extensions and utilities for working with enums
+'''
+
+from   enum import Enum, EnumMeta
+
+# The following class was based in part on the posting by user "Pierre D" at
+# https://stackoverflow.com/a/65225753/743730 made on 2020-12-09.
+
+class MetaEnum(EnumMeta):
+    '''Meta class for enums that implements "contain" test ability.'''
+    def __contains__(cls, item):
+        try:
+            cls(item)
+        except ValueError:
+            return False
+        return True
+
+
+# Inheriting from str solves the problem that PyWebIO otherwise complains about
+# the type values not being json serializable.  This brilliant approach is due
+# to a posting by "Justin Carter" @ https://stackoverflow.com/a/51976841/743730
+
+class ExtendedEnum(str, Enum, metaclass = MetaEnum):
+    '''Extend Enum class with a function allowing a test for containment.'''
+    pass
--- foliage/folio.py
+++ foliage/folio.py
@@ -22,32 +22,11 @@ from   sidetrack import set_debug, log
 from   validators.url import url as valid_url
 
 from   .credentials import Credentials, credentials_complete
+from   .enum_utils import MetaEnum, ExtendedEnum
 
 
 # Public data types.
 # .............................................................................
-
-# The following class was based in part on the posting by user "Pierre D" at
-# https://stackoverflow.com/a/65225753/743730 made on 2020-12-09.
-
-class MetaEnum(EnumMeta):
-    '''Meta class for enums that implements "contain" test ability.'''
-    def __contains__(cls, item):
-        try:
-            cls(item)
-        except ValueError:
-            return False
-        return True
-
-
-# Inheriting from str solves the problem that PyWebIO otherwise complains about
-# the type values not being json serializable.  This brilliant approach is due
-# to a posting by "Justin Carter" @ https://stackoverflow.com/a/51976841/743730
-
-class ExtendedEnum(str, Enum, metaclass = MetaEnum):
-    '''Extend Enum class with a function allowing a test for containment.'''
-    pass
-
 
 class RecordKind(ExtendedEnum):
     ITEM     = 'item'
